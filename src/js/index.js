4aece01b5881a17afe19aee2a5f07aa147f4e87b
SendLessLove <1099762747@qq.com>
2017-04-14 23:08:02 +0800

    add babel-preset-react

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
-document.write('~W~');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>hello world</div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Index />,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,19 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.\js?$/,
+        include: [
+          path.resolve(__dirname, "src/js")
+        ],
+        exclude: [
+          path.resolve(__dirname, "node_modules")
+        ],
+        loader: "babel-loader"
+      }
+    ]
   }
 }
