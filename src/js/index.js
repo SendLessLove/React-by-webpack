25e4158e29f9a061ccbbc118fb7c8001c408c846
SendLessLove <1099762747@qq.com>
2017-04-14 23:13:29 +0800

    del dist and test props

--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,12 @@ import ReactDOM from 'react-dom';
 class Index extends React.Component {
   render() {
     return (
-      <div>hello world</div>
+      <div>hello {this.props.name}</div>
     );
   }
 }
 
 ReactDOM.render(
-  <Index />,
+  <Index name="yusin" />,
   document.getElementById('root')
 );
